0230e85be949a72306e1f9e29548099ad7b30c60
Aryan Gupta <totallymadgamer331@gmail.com>
2026-07-19 14:32:52 +0530

    logging or env variable for testing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,7 @@ import api, { getJobStatus, getDownloadUrl } from "./services/api";
 export default function App() {
 
     const [file, setFile] = useState<File | null>(null);
+    console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
     const [sourceLanguage, setSourceLanguage] = useState("english");
     const [targetLanguage, setTargetLanguage] = useState("hindi");
 
